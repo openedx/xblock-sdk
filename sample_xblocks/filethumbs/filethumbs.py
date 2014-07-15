62a8600f3e75affd08e6bf87f187ad5a92434913
Piotr Mitros <pmitros@mitx.mit.edu>
2014-07-15 13:57:53 -0400

    File thumbs works

--- sample_xblocks/filethumbs/filethumbs.py
+++ sample_xblocks/filethumbs/filethumbs.py
@@ -1,12 +1,25 @@
 """An XBlock providing thumbs-up/thumbs-down voting."""
 
+import json
+import png
+
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, Boolean, Filesystem
 from xblock.fragment import Fragment
 import pkg_resources
 
 import logging
+
 log = logging.getLogger(__name__)
+
+arrow = ["11011", 
+         "10001", 
+         "00000", 
+         "11011", 
+         "11011", 
+         "11011", 
+         "10001"]
+arrow = [map(int, x) for x in arrow]
 
 @XBlock.needs('fs')
 class FileThumbsBlock(XBlock):
@@ -20,8 +33,8 @@ class FileThumbsBlock(XBlock):
 
     """
 
-    upvotes = Integer(help="Number of up votes", default=0, scope=Scope.user_state_summary)
-    downvotes = Integer(help="Number of down votes", default=0, scope=Scope.user_state_summary)
+    upvotes = 0 #Integer(help="Number of up votes", default=0, scope=Scope.user_state_summary)
+    downvotes = 0 # Integer(help="Number of down votes", default=0, scope=Scope.user_state_summary)
     voted = Boolean(help="Has this student voted?", default=False, scope=Scope.user_state)
     fs = Filesystem(help="File system", scope=Scope.user_state)
 
@@ -38,6 +51,15 @@ class FileThumbsBlock(XBlock):
         html_str = pkg_resources.resource_string(__name__, "static/html/thumbs.html")
         frag = Fragment(unicode(html_str).format(self=self))
 
+        if not self.fs.exists("votes.json"):
+            f = self.fs.open("votes.json", "wb")
+            json.dump({'up':0, 'down':0}, f)
+            f.close()
+
+        votes = json.load(self.fs.open("votes.json"))
+        self.upvotes = votes['up']
+        self.downvotes = votes['down']
+            
         # Load the CSS and JavaScript fragments from within the package
         css_str = pkg_resources.resource_string(__name__, "static/css/thumbs.css")
         frag.add_css(unicode(css_str))
@@ -46,11 +68,18 @@ class FileThumbsBlock(XBlock):
                                                "static/js/src/thumbs.js")
         frag.add_javascript(unicode(js_str))
 
-        f = self.fs.open("hello.txt", "w")
-        f.write(u"Hello")
+        f = self.fs.open("uparrow.png", "wb")
+        png.Writer(len(arrow[0]), len(arrow), greyscale = True, bitdepth = 1).write(f, arrow)
+        f.close()
+        f = self.fs.open("downarrow.png", "wb")
+        png.Writer(len(arrow[0]), len(arrow), greyscale = True, bitdepth = 1).write(f, arrow[::-1])
         f.close()
 
-        frag.initialize_js('FileThumbsBlock')
+        frag.initialize_js('FileThumbsBlock', {'up' : self.upvotes, 
+                                               'down' : self.downvotes, 
+                                               'voted': self.voted, 
+                                               'uparrow' : self.fs.get_url('uparrow.png'), 
+                                               'downarrow' : self.fs.get_url('downarrow.png')})
         return frag
 
     problem_view = student_view
@@ -67,6 +96,10 @@ class FileThumbsBlock(XBlock):
         #         log.error("cheater!")
         #         return
 
+        votes = json.load(self.fs.open("votes.json"))
+        self.upvotes = votes['up']
+        self.downvotes = votes['down']
+
         if data['voteType'] not in ('up', 'down'):
             log.error('error!')
             return
@@ -75,6 +108,10 @@ class FileThumbsBlock(XBlock):
             self.upvotes += 1
         else:
             self.downvotes += 1
+
+        f = self.fs.open("votes.json", "wb")
+        json.dump({'up':self.upvotes, 'down':self.downvotes}, f)
+        f.close()
 
         self.voted = True
 
--- sample_xblocks/filethumbs/static/js/src/thumbs.js
+++ sample_xblocks/filethumbs/static/js/src/thumbs.js
@@ -1,9 +1,14 @@
-function FileThumbsBlock(runtime, element) {
+function FileThumbsBlock(runtime, element, data) {
     function updateVotes(votes) {
         $('.upvote .count', element).text(votes.up);
         $('.downvote .count', element).text(votes.down);
     }
 
+    $('.upvote .arrow', element).html('<img height="14" width="10" src="'+data.uparrow+'"/>');
+    $('.downvote .arrow', element).html('<img height="14" width="10" src="'+data.downarrow+'"/>');
+
+    updateVotes(data)
+    
     var handlerUrl = runtime.handlerUrl(element, 'vote');
 
     $('.upvote', element).click(function(eventObject) {
