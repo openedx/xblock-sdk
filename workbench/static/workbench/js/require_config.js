0cb8fc5cde5d0857a51dd09bcd7e36c55394e1f7
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-10-15 21:48:26 +0300

    Adding jquery to paths

--- workbench/static/workbench/js/require_config.js
+++ workbench/static/workbench/js/require_config.js
@@ -1,7 +1,8 @@
 var require = {
     baseUrl: "/static",
     paths: {
-        'xblock_resource': '../resource'
+        'xblock_resource': '../resource',
+        'jquery': 'workbench/js/vendor/jquery.min'
     },
     waitSeconds: 15
 };
