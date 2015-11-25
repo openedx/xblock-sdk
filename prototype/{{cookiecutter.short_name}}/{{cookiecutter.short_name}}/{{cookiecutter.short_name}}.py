005484d44bf2c600c44a31aa3f69a031c65c67f1
Lyla Fischer <lylafisch@gmail.com>
2015-11-25 08:48:08 -0500

    multiple default scenarios

--- prototype/{{cookiecutter.short_name}}/{{cookiecutter.short_name}}/{{cookiecutter.short_name}}.py
+++ prototype/{{cookiecutter.short_name}}/{{cookiecutter.short_name}}/{{cookiecutter.short_name}}.py
@@ -59,5 +59,12 @@ class {{cookiecutter.class_name}}(XBlock):
         """A canned scenario for display in the workbench."""
         return [
             ("{{cookiecutter.class_name}}",
-            """<{{cookiecutter.short_name|lower}}/>"""),
+            """<{{cookiecutter.short_name|lower}}/>"""),\
+            ("Multiple {{cookiecutter.class_name}}",
+             """<vertical_demo>
+                <{{cookiecutter.short_name|lower}}/>
+                <{{cookiecutter.short_name|lower}}/>
+                <{{cookiecutter.short_name|lower}}/>
+                </vertical_demo>
+             """),
         ]
