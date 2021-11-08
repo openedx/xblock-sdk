39b1a5ca64d9cf605f852f85a7d8e79a50e047b5
Rebecca Graber <rgraber@edx.org>
2021-11-08 13:23:09 -0500

    fix: rm extraneous parens

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ def is_requirement(line):
     # UPDATED VIA SEMGREP - if you need to remove/modify this method remove this line and add a comment specifying why
 
     return line and line.strip() and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
-    )
 
 
 def load_requirements(*requirements_paths):
